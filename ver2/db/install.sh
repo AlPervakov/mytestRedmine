750012d83bf04bae673d938a9b361beeeb662d99
AlPervakov <36540048+AlPervakov@users.noreply.github.com>
2020-02-08 14:00:19 +0300

    Update install.sh

--- ver2/db/install.sh
+++ ver2/db/install.sh
@@ -2,7 +2,7 @@
 # Install core system packages
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y mysql-server ruby-full
+apt-get install -y mysql-server
 
 service mysql start
 sleep 2
@@ -10,7 +10,3 @@ sleep 2
 mysql -u root --execute="CREATE DATABASE redmine character SET utf8;"
 mysql -u root --execute="CREATE user 'redmine'@'localhost' IDENTIFIED BY 'redmine';"
 mysql -u root --execute="GRANT ALL privileges ON redmine.* TO 'redmine'@'localhost';"
-
-rake generate_secret_token
-RAILS_ENV=production rake db:migrate
-RAILS_ENV=production REDMINE_LANG=ru rake redmine:load_default_data
