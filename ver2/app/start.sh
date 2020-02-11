606773a56d34c4a9b84964ebdf029e0c5b3bdfe3
AlPervakov <36540048+AlPervakov@users.noreply.github.com>
2020-02-11 15:25:21 +0300

    Update start.sh

--- ver2/app/start.sh
+++ ver2/app/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 service nginx start
+cd /var/data/redmine
 RAILS_ENV=production rake db:migrate
 RAILS_ENV=production REDMINE_LANG=en rake redmine:load_default_data
 tail -F /var/log/nginx/error.log
